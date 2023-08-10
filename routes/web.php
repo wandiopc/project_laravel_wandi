ec2a535712fe90c4019120c80c2e58053776724c
rifaldo-dev <muhammadrifaldosaputra@gmail.com>
2023-08-10 11:42:38 +0700

    update database/login

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Backend\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,4 +17,15 @@ use App\Http\Controllers\Backend\DashboardController;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/dashboard' , [ DashboardController::class,'Dashboard'])->name('dashboard');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
